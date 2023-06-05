75319a3c9363297747f9ac1ba9e0c1488d61309b
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-05 22:34:06 +0300

    Update ContactForm.styled.jsx

---
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const Form = styled.form`
+  display: inline-flex;
+  flex-direction: column;
+  padding: 20px;
+  padding-right: 200px;
+  border: 1px solid #000;
+`;
+
+export const FormLabel = styled.label`
+  margin-bottom: 10px;
+  font-size: 17px;
+  font-weight: 500;
+`;
+
+export const Input = styled.input`
+  margin-bottom: 25px;
+`;
