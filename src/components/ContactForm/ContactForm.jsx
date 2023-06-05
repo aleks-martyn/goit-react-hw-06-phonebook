a19ed648060970b327bf2397cccded15aa79a43d
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-05 23:19:45 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,9 +4,28 @@ import { nanoid } from '@reduxjs/toolkit';
 import { Form, FormLabel, Input } from './ContactForm.styled';
 
 export const ContactForm = () => {
+  const dispatch = useDispatch();
+
+  const nameInputId = nanoid();
+  const numberInputId = nanoid();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.currentTarget;
+
+    const contact = {
+      id: nanoid(),
+      name: form.elements.name.value,
+      number: form.elements.number.value,
+    };
+
+    dispatch(addContact(contact));
+    form.reset();
+  };
+
   return (
     <Form onSubmit={handleSubmit}>
-      <FormLabel htmlFor={nanoid()}>
+      <FormLabel htmlFor={nameInputId}>
         <span>Name</span>
       </FormLabel>
       <Input
@@ -15,11 +34,10 @@ export const ContactForm = () => {
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
-        onChange={handleChange}
-        id={nanoid()}
+        id={nameInputId}
       />
 
-      <FormLabel htmlFor={nanoid()}>
+      <FormLabel htmlFor={numberInputId}>
         <span>Number</span>
       </FormLabel>
       <Input
@@ -28,8 +46,7 @@ export const ContactForm = () => {
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
-        onChange={handleChange}
-        id={nanoid()}
+        id={numberInputId}
       />
 
       <button type="submit">Add contact</button>
