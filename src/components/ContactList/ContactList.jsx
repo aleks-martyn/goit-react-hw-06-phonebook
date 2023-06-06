07de64e0df4f0fbabedfa18b682473e3b49fe552
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-07 00:51:35 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,4 +1,4 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { ContactListItem } from 'components/ContactListItem';
 import { getContacts, getFilter } from 'redux/selectors';
 import { List } from './ContactList.styled';
@@ -7,9 +7,19 @@ export const ContactList = () => {
   const contacts = useSelector(getContacts);
   const filter = useSelector(getFilter);
 
+  const getFilteredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const filteredContacts = getFilteredContacts();
+
   return (
     <List>
-      {users.map(({ id, name, number }) => (
+      {filteredContacts.map(({ id, name, number }) => (
         <ContactListItem key={id} name={name} number={number} />
       ))}
     </List>
