d7aec4b2f2a074d8326fe620b1bfcb6edb3a4f3d
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-07 00:25:52 +0300

    Update ContactListItem.jsx

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -2,16 +2,16 @@ import { useDispatch } from 'react-redux';
 import { deleteContact } from 'redux/contactsSlice';
 import { Item, Text, Marker } from './ContactListItem.styled';
 
-export const ContactListItem = ({ userId, userName, userNumber }) => {
+export const ContactListItem = ({ id, name, number }) => {
   const dispatch = useDispatch();
 
-    const handleDelete = () => dispatch(deleteContact(userId))
+  const handleDelete = () => dispatch(deleteContact(id));
 
   return (
     <Item>
       <Marker></Marker>
       <Text>
-        {userName}: <span>{userNumber}</span>
+        {name}: <span>{number}</span>
       </Text>
       <button type="button" onClick={handleDelete}>
         Delete
