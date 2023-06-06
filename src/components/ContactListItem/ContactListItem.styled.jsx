086d3cced65211b0bb1cc8d0d22c7e0c882c9694
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-07 00:02:55 +0300

    Update ContactListItem.styled.jsx

---
+++ src/components/ContactListItem/ContactListItem.styled.jsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const Item = styled.li`
+  display: flex;
+  gap: 15px;
+  align-items: center;
+`;
+
+export const Text = styled.p`
+  font-size: 17px;
+  font-weight: 500;
+`;
+
+export const Marker = styled.span`
+  width: 5px;
+  height: 5px;
+  background-color: #000;
+  border-radius: 50%;
+`;
