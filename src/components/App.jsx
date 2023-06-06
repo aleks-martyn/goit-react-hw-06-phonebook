9cd74354e19a9489a44f8eab04c9c15d8020216f
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-06 22:54:40 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
+import { ContactForm } from './ContactForm';
 import { Container } from './App.styled';
 
 export const App = () => {
   return (
     <Container>
       <h1>Phonebook</h1>
+      <ContactForm />
       <h2>Contacts</h2>
     </Container>
   );
