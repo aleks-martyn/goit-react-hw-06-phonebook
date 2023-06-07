094b4d90fe777a7ae76cd1e38e6ce7a705264aa5
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-07 15:35:23 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { ContactForm } from './ContactForm';
 import { ContactList } from './ContactList';
+import { Filter } from './Filter';
 import { Container } from './App.styled';
 
 export const App = () => {
@@ -8,6 +9,7 @@ export const App = () => {
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
+      <Filter />
       <ContactList />
     </Container>
   );
