0e66231b440885968d9e07275e25156d0b2ba6e1
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-07 15:02:27 +0300

    Update Filter.styled.jsx

---
+++ src/components/Filter/Filter.styled.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const FilterLabel = styled.label`
+  display: inline-flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Text = styled.span`
+  font-size: 17px;
+  font-weight: 500;
+`;
