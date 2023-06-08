9d1810ebc0958408ba00aecc22f638d762e50903
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-08 23:21:25 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,2 @@
-export const getContacts = state => state.contacts;
-export const getFilter = state => state.filter;
+export const getContacts = ({ contacts }) => contacts;
+export const getFilter = ({ filter }) => filter;
