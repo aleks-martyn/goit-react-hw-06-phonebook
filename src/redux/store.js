32d0a9df8dafe9041bf591e86dd9eecafd7865cd
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-08 23:46:17 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistReducer(persistConfig, contactsReducer),
     filter: filterReducer,
   },
 });
+
+export const persistor = persistStore(store);
