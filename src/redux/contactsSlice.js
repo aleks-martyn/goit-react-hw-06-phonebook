c20190ad5a3ef4904aa6db68cad9851ac5fbd130
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-09 16:02:17 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,4 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const initialState = {
   contacts: [
@@ -15,7 +17,6 @@ const contactsSlice = createSlice({
   reducers: {
     addContact(state, { payload }) {
       state.contacts = [...state.contacts, payload];
-      console.log(state.contacts);
     },
     deleteContact(state, { payload }) {
       state.contacts = state.contacts.filter(({ id }) => id !== payload);
@@ -23,5 +24,13 @@ const contactsSlice = createSlice({
   },
 });
 
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+
+export const contactsReducer = persistReducer(
+  persistConfig,
+  contactsSlice.reducer
+);
 export const { addContact, deleteContact } = contactsSlice.actions;
-export const contactsReducer = contactsSlice.reducer;
